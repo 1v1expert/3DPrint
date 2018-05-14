5b28b218098672c00bffc42a6341771c86d0f2a0
1v1expert <1v1expert@gmail.com>
2018-05-14 15:33:50 +0300

    Port from 5005 to 5000

--- Jetson/html/full-width-dark/dist/js/init.js
+++ Jetson/html/full-width-dark/dist/js/init.js
@@ -17,7 +17,7 @@
 var ApiKeyDev = "E39CDD5E459A4493A6AC51204115204D";
 var ApiKeyProd ="083E87CC0E9E4300AA7354F31C8FD6F8";
 var ActiveApi = ApiKeyProd;
-var PortProd = 5005;
+var PortProd = 5000;
 var PortTest = 8112;
 var ActivePort = PortProd;
 
