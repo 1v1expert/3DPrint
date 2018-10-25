3c99beccb4395da05b41cd97bf55fbdfe6af4b7b
1v1expert <1v1expert@gmail.com>
2018-10-26 00:56:58 +0300

    Fix config

--- config/config.js
+++ config/config.js
@@ -1,4 +1,21 @@
 translate_state = {
     Operational: "Готов к печати",
     Printing: "Идёт печать"
-};
+},
+buttons = {
+    General:
+    [
+        {
+            name: "Калибровка",
+            command: '{"commands": ["M206 Z0", "M666 X0 Y0 Z0", "G32", "G31", "G28", "G1 Z22.1 F2000", "G30 Y0", "M374", "M500", "G28"]}'
+        },
+        {
+            name: "M999",
+            command: '{"command": "M999"}'
+        }
+    ],
+    Aditional:
+    [
+
+    ]
+}
