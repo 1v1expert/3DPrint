d7176d87ae8e55f0c5d6d6e0b262dc5660f3a98a
1v1expert <1v1expert@gmail.com>
2018-11-08 00:17:09 +0300

    Update configuration

---
+++ Jetson/html/full-width-dark/dist/js/handlers/loading_buttons.js
@@ -0,0 +1,8 @@
+/* Processing data module */
+/* This file is part of the 3DPrinter Project
+
+   __author__ = 'Sazonov Vladislav Sergeevich <1v1expert@gmail.com>'
+   __copyright__ = 'Copyright (C) 2018 VLADDOS'
+   __license__ = 'GNU General Public License v2 http://www.gnu.org/licenses/gpl2.html'
+*/
+
