f8989c843048e52bc20b984b88a8ed72bbda4474
1v1expert <1v1expert@gmail.com>
2019-10-05 22:09:23 +0300

    fix

--- app/static/main/js/events/methods.js
+++ app/static/main/js/events/methods.js
@@ -101,9 +101,9 @@ function CustomGetFiles() {
             "method": "GET"
     }).done(function (response){
         console.log(response);
-        if !(JSON.parse(response).files){
-            swal('', 'Файлов не обнаружено', 'info')
-        }
+        if (typeof JSON.parse(response).files.length === 'undefined'){
+            swal('', 'Файлов не обнаружено', 'info');
+        };
         _.each(JSON.parse(response).files, function(entry) {
             data_html = data_html + "<div class='col-lg-3 col-md-3 col-sm-3 col-xs-12  file-box'><div class='file'><a onclick=\"ConfirmCopy(\'" + entry.path + "\' , \'" + entry.name + "\') \" > " +
                 // "<div class='icon'> <i class='zmdi zmdi-file-text'></i> </div> " +
